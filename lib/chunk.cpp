f584aaf882857faf6ea4e7f4b1dffc077a9361cd
Ingo Luetkebohle <iluetkeb@techfak.uni-bielefeld.de>
2011-04-24 00:27:01 +0200

    add chunk.hpp and enable newer c++ standard

---
+++ lib/chunk.cpp
@@ -0,0 +1,39 @@
+#include "tidelog.hpp"
+#include "chunk.hpp"
+
+#include <cstdint>
+
+namespace tide {
+    namespace log {
+        ChunkEntry::ChunkEntry(const uint64_t timestamp, const Array& data) : timestamp(timestamp), length(data.length) {
+        }            
+        uint64_t ChunkEntry::get_timestamp() const {
+            return timestamp;
+        }
+            
+        uint64_t ChunkEntry::get_length() const {
+            return length;
+        }
+        
+        Chunk::Chunk(const int id, const off_t start) : id(id), start_filepos(start),
+                start_timestamp(UINT64_MAX), end_timestamp(0), chunk_length(0), num_entries(0) {
+            
+        }
+        void Chunk::update(const ChunkEntry& entry) {
+            if(entry.get_timestamp() < start_timestamp)
+                start_timestamp = entry.get_timestamp();
+            if(entry.get_timestamp() > end_timestamp)
+                end_timestamp = entry.get_timestamp();
+            chunk_length += entry.get_length();
+            ++num_entries;
+        }
+            
+        uint64_t Chunk::get_size() {
+            return this->chunk_length;
+        }
+        CHUNK Chunk::get_header() const {
+            return CHUNK(id, num_entries, start_timestamp, end_timestamp, 0);
+        }
+
+    }
+}
---
+++ lib/chunk.hpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   chunk.hpp
+ * Author: ingo
+ *
+ * Created on 22. April 2011, 23:46
+ */
+
+#ifndef CHUNK_HPP
+#define	CHUNK_HPP
+
+#include "tidestruct.hpp"
+
+namespace tide {
+    namespace log {
+        class ChunkEntry {
+        private:
+            const uint64_t timestamp;
+            const uint64_t length;
+            
+        public:
+            ChunkEntry(const uint64_t timestamp, const Array& data);
+            
+            uint64_t get_timestamp() const;
+            uint64_t get_length() const;
+        };
+        
+        class Chunk {
+        private:
+            const int id;
+            const off_t start_filepos;
+            uint64_t start_timestamp, end_timestamp;
+            uint64_t chunk_length;
+            unsigned int num_entries;
+            
+        public:
+            Chunk(const int id, const off_t start);
+            void update(const ChunkEntry& entry);
+            
+            uint64_t get_size();
+            CHUNK get_header() const;
+        };
+    }
+}
+
+#endif	/* CHUNK_HPP */
+
